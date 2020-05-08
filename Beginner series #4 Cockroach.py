ae9e804c8b5726af11680e62198824d1efa72a78
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-08 18:35:12 +0530

    Create Beginner series #4 Cockroach.py

---
+++ Beginner series #4 Cockroach.py
@@ -0,0 +1,7 @@
+def cockroach_speed(s):
+    return  int((s*500)/18)
+    
+#test.describe("Basic Tests")
+#test.assert_equals(cockroach_speed(1.08),30)
+#test.assert_equals(cockroach_speed(1.09),30)
+#test.assert_equals(cockroach_speed(0),0)
