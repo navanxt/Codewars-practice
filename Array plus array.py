7ffc484eb1e3723fd8a719d413290cfe5071f551
Navanit Nandakumar <navanitnandakumar@gmail.com>
2020-05-17 19:39:31 +0530

    Update Array plus array.py

--- Array plus array.py
+++ Array plus array.py
@@ -1,8 +1,5 @@
 def array_plus_array(arr1,arr2):
-    sum = int(0)
-    for i in range(len(arr1)):
-        sum = sum + arr1[i] + arr2[i]
-    return sum
+    return sum(arr1) + sum(arr2)
     
 #Test.it("Basic test")
 #Test.assert_equals(array_plus_array([1, 2, 3], [4, 5, 6]), 21)
